1cda4570318ce49022d814589d75d1bca293f740
Carlos <carloseduardo.silva@qwasolucoes.com.br>
2024-09-10 14:58:23 -0300

    Update CourseApplication.java

--- course/src/main/java/com/educaweb/course/CourseApplication.java
+++ course/src/main/java/com/educaweb/course/CourseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseApplication {
-
+//TODO
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 	}
