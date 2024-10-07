00af6ade2a93a8648db29295e261077eee30180b
Carlos <kadudu23@gmail.com>
2024-10-07 14:57:28 -0300

    exception hadler delete

--- course/src/main/java/com/educaweb/course/config/TestConfig.java
+++ course/src/main/java/com/educaweb/course/config/TestConfig.java
@@ -62,12 +62,13 @@ public class TestConfig implements CommandLineRunner {
 
 		User u1 = new User(null, "Maria Joaquina", "maria@gmail.com", "9888888", "123456");
 		User u2 = new User(null, "Valdevino João", "joao@gmail.com", "98665", "987654");
+		User u3 = new User(null, "mario João", "joao@gmail.com", "9687", "958756");
 
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(u1, u2,u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
--- course/src/main/java/com/educaweb/course/services/UserService.java
+++ course/src/main/java/com/educaweb/course/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educaweb.course.entities.User;
 import com.educaweb.course.repositories.UserRepository;
+import com.educaweb.course.services.exceptions.DatabaseException;
 import com.educaweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,6 +34,8 @@ public class UserService {
 		 repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
 		}
 	}
 	public User update(Long id, User obj) {
