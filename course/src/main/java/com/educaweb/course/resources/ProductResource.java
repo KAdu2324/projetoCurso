5091384e592b61cb1a51fa42ecc0095734f07c74
Carlos <kadudu23@gmail.com>
2024-10-07 16:55:20 -0300

    Ajustando edição e exclusão do produto

--- course/src/main/java/com/educaweb/course/resources/ProductResource.java
+++ course/src/main/java/com/educaweb/course/resources/ProductResource.java
@@ -1,16 +1,23 @@
 package com.educaweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educaweb.course.entities.Product;
+import com.educaweb.course.entities.User;
 import com.educaweb.course.services.ProductService;
 
 @RestController
@@ -32,5 +39,25 @@ public class ProductResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product obj){
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 }
